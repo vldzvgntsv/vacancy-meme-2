66f20502180563b8f5141a0ef7147ae2609c9b88
vldzvgntsv <tetrogum@gmail.com>
2022-05-31 15:41:23 +0300

    heroku deploy

--- conf/settings.py
+++ conf/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fwe*$of*qm2_146s^n@9zyp7d6@u)y!0#*d+_#k2)9^%*m2i8a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
